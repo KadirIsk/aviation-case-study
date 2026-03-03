bcb17c1ba294c505d51c6caf3415cbc07d6a438f
Kadir IŞIK <isik.kadr@gmail.com>
2026-03-03 15:18:23 +0300

    [issue-number] initial commit

---
+++ src/main/java/com/aviation/routing/flight_path_engine/FlightPathEngineApplication.java
@@ -0,0 +1,13 @@
+package com.aviation.routing.flight_path_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPathEngineApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(FlightPathEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aviation/routing/flight_path_engine/FlightPathEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aviation.routing.flight_path_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPathEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
