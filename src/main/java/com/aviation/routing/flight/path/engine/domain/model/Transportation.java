e4b4cc9f778e543ee27c13d03079f46db1e97141
Kadir IŞIK <isik.kadr@gmail.com>
2026-03-04 15:33:35 +0300

    [issue-number] equals'i id ustunden yapmak tehlikeli ama O(1)'dan vazgecmek istemiyorum

--- src/main/java/com/aviation/routing/flight/path/engine/domain/model/Transportation.java
+++ src/main/java/com/aviation/routing/flight/path/engine/domain/model/Transportation.java
@@ -2,10 +2,12 @@ package com.aviation.routing.flight.path.engine.domain.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@EqualsAndHashCode(of = {"id"})
 @Builder
 @Getter
 @Setter
