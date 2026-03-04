5427583ab59acbc8d00f64740afa107287f5213b
Kadir IŞIK <isik.kadr@gmail.com>
2026-03-04 14:27:30 +0300

    [issue-number] rename

--- src/main/java/com/aviation/routing/flight/path/engine/infrastructure/persistence/MultimodalRuleBasedGraphProvider.java
+++ src/main/java/com/aviation/routing/flight/path/engine/infrastructure/persistence/MultimodalRuleBasedGraphProvider.java
@@ -5,7 +5,7 @@ import com.aviation.routing.flight.path.engine.domain.model.route.finder.Graph;
 import com.aviation.routing.flight.path.engine.domain.service.TransitGraphProvider;
 import com.aviation.routing.flight.path.engine.domain.service.dto.GraphProviderRequest;
 
-public class MultimodalRuleBasedTransitProvider implements TransitGraphProvider<Location> {
+public class MultimodalRuleBasedGraphProvider implements TransitGraphProvider<Location> {
 
     @Override
     public Graph<Location> createGraph(GraphProviderRequest request) {
