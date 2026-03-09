09aa0e405a729519b0da6506780abb5fa3dd8dcf
Kadir IŞIK <isik.kadr@gmail.com>
2026-03-09 08:38:37 +0300

    [issue-number] docker ortami icin application.yaml dosyasi

--- src/main/java/com/aviation/routing/flight/path/engine/infrastructure/config/ProfileConstants.java
+++ src/main/java/com/aviation/routing/flight/path/engine/infrastructure/config/ProfileConstants.java
@@ -7,6 +7,7 @@ public class ProfileConstants {
     public static final String LOCAL = "local";
     public static final String TEST = "test";
     public static final String PROD = "prod";
+    public static final String DOCKER = "docker";
     
     public static final String NOT_LOCAL = "!" + LOCAL;
     public static final String NOT_TEST = "!" + TEST;
